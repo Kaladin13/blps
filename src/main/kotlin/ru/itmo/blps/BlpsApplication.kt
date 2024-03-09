0dedbb9cd452186d88db993f8cc6e0a9baa1f13c
kaladin <335095@niuitmo.ru>
2024-03-09 18:32:24 +0300

    Initial commit

---
+++ src/main/kotlin/ru/itmo/blps/BlpsApplication.kt
@@ -0,0 +1,11 @@
+package ru.itmo.blps
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlpsApplication
+
+fun main(args: Array<String>) {
+	runApplication<BlpsApplication>(*args)
+}
