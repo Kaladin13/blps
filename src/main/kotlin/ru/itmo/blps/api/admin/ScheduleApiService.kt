4ec15da735ce8aa41f3e25f0d5f7f61cb9b540fe
rusilee <ildarelmeev@yandex>
2024-03-10 19:46:50 +0300

    fix type in api

--- src/main/kotlin/ru/itmo/blps/api/admin/ScheduleApiService.kt
+++ src/main/kotlin/ru/itmo/blps/api/admin/ScheduleApiService.kt
@@ -1,11 +1,13 @@
 package ru.itmo.blps.api.admin
 
 import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Component
 import ru.itmo.blps.generated.api.admin.ScheduleDraftApiDelegate
 import ru.itmo.blps.generated.model.admin.ScheduleDraft
 import ru.itmo.blps.generated.model.admin.ScheduleDraftCreationRequest
 import ru.itmo.blps.generated.model.admin.UpdateScheduleDraftStatus
 
+@Component
 class ScheduleApiService : ScheduleDraftApiDelegate {
     override fun createScheduleDraft(
             scheduleDraftCreationRequest: ScheduleDraftCreationRequest
@@ -17,7 +19,9 @@ class ScheduleApiService : ScheduleDraftApiDelegate {
         return super.getAllScheduleDrafts()
     }
 
-    override fun updateScheduleDraftStatus(updateScheduleDraftStatus: MutableList<UpdateScheduleDraftStatus>?): ResponseEntity<Void> {
+    override fun updateScheduleDraftStatus(
+            updateScheduleDraftStatus: MutableList<UpdateScheduleDraftStatus>?
+    ): ResponseEntity<Void> {
         return super.updateScheduleDraftStatus(updateScheduleDraftStatus)
     }
 }
