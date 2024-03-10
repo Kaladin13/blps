db0c573d97ec1dd41c0b72cfe4897b468dbd8ab4
kaladin <335095@niuitmo.ru>
2024-03-10 17:13:54 +0300

    add pingpong

---
+++ src/main/kotlin/ru/itmo/blps/api/user/PingApiService.kt
@@ -0,0 +1,17 @@
+package ru.itmo.blps.api.user
+
+import lombok.RequiredArgsConstructor
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Component
+import ru.itmo.blps.generated.api.user.PingApiDelegate
+import ru.itmo.blps.generated.model.user.PingResponse
+
+@Component
+@RequiredArgsConstructor
+class PingApiService : PingApiDelegate {
+
+    override fun ping(): ResponseEntity<PingResponse> {
+        return ResponseEntity(PingResponse().pong("xxx"), HttpStatus.OK)
+    }
+}
