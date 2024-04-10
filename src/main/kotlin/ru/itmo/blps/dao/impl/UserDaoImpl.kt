e92b74fd3d54980c8d7d188a3d14ad822f5b84ae
rusilee <ildarelmeev@yandex>
2024-04-10 11:20:39 +0300

    add user dao

--- src/main/kotlin/ru/itmo/blps/dao/impl/UserDaoImpl.kt
+++ src/main/kotlin/ru/itmo/blps/dao/impl/UserDaoImpl.kt
@@ -1,10 +1,20 @@
 package ru.itmo.blps.dao.impl
 
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.dataformat.xml.XmlMapper
+import org.springframework.stereotype.Repository
 import ru.itmo.blps.dao.UserDao
 import ru.itmo.blps.model.User
 
+@Repository
 class UserDaoImpl : UserDao {
+
     override fun getByUsername(username: String): User? {
-        TODO("Not yet implemented")
+        val users = XmlMapper().readValue(stream, object : TypeReference<List<User>>() {}) ?: emptyList()
+        return users.filter { it.username == username }.firstOrNull()
+    }
+
+    companion object {
+        private val stream = UserDaoImpl::class.java.getResource("security/users.xml")
     }
 }
